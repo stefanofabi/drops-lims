5b31bc6df414fa2f9636305fa9aa6b790088f82f
Stefano Fabi <stefanofabi96@gmail.com>
2020-04-16 05:42:10 -0300

    test data generation corrected

--- database/factories/PatientsFactory.php
+++ database/factories/PatientsFactory.php
@@ -6,23 +6,64 @@ use App\Patient;
 use Faker\Generator as Faker;
 
 $factory->define(Patient::class, function (Faker $faker) {
-    return [
-        //
-        'full_name' => $faker->lastName()." ". $faker->name(),
-        'key' => $faker->randomNumber(8),
-        'sex' => $faker->randomElement($array = array('F', 'M')),
-        'birth_date' => $faker->date($format = 'Y-m-d', $max = 'now'),
-        'city' => $faker->city,
-        'address' => $faker->streetAddress, 
 
-        // for animals
-        'owner' => $faker->name()." ".$faker->lastName(),
+	$type = $faker->randomElement(array('animal', 'human', 'industrial'));
+	switch ($type) {
+		case 'animal': {
+		    $array = [
+		        //
+		        'full_name' => $faker->lastName()." ". $faker->name(),
+		        'key' => $faker->randomNumber(8),
+		        'sex' => $faker->randomElement($array = array('F', 'M')),
+		        'birth_date' => $faker->date($format = 'Y-m-d', $max = 'now'),
+		        'city' => $faker->city,
+		        'address' => $faker->streetAddress, 
 
-        // for industrials
-        'business_name' => $faker->name(),
-		'tax_condition' => $faker->randomElement(array('Exempt', 'Monotax', 'Registered Responsible')),   
-		'start_activity' => $faker->date($format = 'Y-m-d', $max = 'now'),
+		        // for animals
+		        'owner' => $faker->name()." ".$faker->lastName(),
 
-        'type' => $faker->randomElement(array('animal', 'human', 'industrial')),
-    ];
+		        // for industrials
+		        'business_name' => $faker->name(),
+				'tax_condition' => $faker->randomElement(array('Exempt', 'Monotax', 'Registered Responsible')),   
+				'start_activity' => $faker->date($format = 'Y-m-d', $max = 'now'),
+
+		        'type' => $type,
+		    ];
+		}
+
+		case 'human': {
+			$array = [
+		        //
+		        'full_name' => $faker->lastName()." ". $faker->name(),
+		        'key' => $faker->randomNumber(8),
+		        'sex' => $faker->randomElement($array = array('F', 'M')),
+		        'birth_date' => $faker->date($format = 'Y-m-d', $max = 'now'),
+		        'city' => $faker->city,
+		        'address' => $faker->streetAddress, 
+		        
+		        'type' => $type,
+		    ];
+		} 
+
+		case 'industrial': {
+			$array = [
+		        //
+		        'full_name' => $faker->lastName()." ". $faker->name(),
+		        'key' => $faker->randomNumber(8),
+		        'sex' => $faker->randomElement($array = array('F', 'M')),
+		        'birth_date' => $faker->date($format = 'Y-m-d', $max = 'now'),
+		        'city' => $faker->city,
+		        'address' => $faker->streetAddress, 
+
+		        // for industrials
+		        'business_name' => $faker->name(),
+				'tax_condition' => $faker->randomElement(array('Exempt', 'Monotax', 'Registered Responsible')),   
+				'start_activity' => $faker->date($format = 'Y-m-d', $max = 'now'),
+
+		        'type' => $type,
+		    ];
+		}
+	}
+
+	return $array;
 });
