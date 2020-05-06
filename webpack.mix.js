5a78dcce93581fde885d024f128d0553d56ec94a
Stefano <stefanofabi96@gmail.com>
2020-05-06 19:00:29 -0300

    changes in views and update jquery

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,3 +13,5 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css');
+
+mix.copy('node_modules/popper.js/dist/popper.js.map', 'public/js/popper.js.map');
