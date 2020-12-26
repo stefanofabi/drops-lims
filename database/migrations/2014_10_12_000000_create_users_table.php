08badbf68eddeeea889a1875e6d0587950b236ef
stefanofabi <stefano.fabi@est.fi.uncoma.edu.ar>
2020-12-26 15:48:39 -0300

    Delete is_admin attribute

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,6 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->boolean('is_admin')->default(false);
             $table->string('password');
             $table->rememberToken();
             
