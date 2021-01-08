385f006e84968f9fd1143c03cf1745767d5580e2
stefanofabi <stefanofabi96@gmail.com>
2021-01-08 05:08:13 -0300

    Update head.blade.php

--- resources/views/head.blade.php
+++ resources/views/head.blade.php
@@ -19,3 +19,14 @@
 
 <!-- Copyright -->
 <meta name="copyright" content="SrLab" />
+
+<!-- Used by ajax -->
+<meta name="csrf-token" content="{{ csrf_token() }}">
+
+<script type="text/javascript">
+    $.ajaxSetup({
+        headers: {
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        }
+    });
+</script>
