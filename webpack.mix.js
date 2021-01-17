c60a4a018414c5ec196edd172040708e783cf659
stefanofabi <stefanofabi96@gmail.com>
2021-01-17 17:34:28 -0300

    Update webpack.mix.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -6,13 +6,17 @@ const mix = require('laravel-mix');
  |--------------------------------------------------------------------------
  |
  | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
+ | for your Laravel applications. By default, we are compiling the CSS
  | file for the application as well as bundling up all the JS files.
  |
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+    .postCss('resources/css/app.css', 'public/css', [
+        //
+    ]);
+	
+mix.sass('resources/sass/app.scss', 'public/css');
 
 mix.copy('node_modules/popper.js/dist/popper.js.map', 'public/js/popper.js.map');
 
