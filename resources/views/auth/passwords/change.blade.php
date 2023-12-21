ac7c553b208ea8ed05546dfdc1a1981d0053717a
Stefano <stefanofabi96@gmail.com>
2023-12-21 17:04:09 -0300

    Change input type to password

--- resources/views/auth/passwords/change.blade.php
+++ resources/views/auth/passwords/change.blade.php
@@ -42,7 +42,7 @@
         <div class="col-md-6">
             <div class="form-group mt-2">
                 <label for="current_password"> {{ trans('auth.current_password') }} </label>
-                <input type="text" class="form-control @error('current_password') is-invalid @enderror" name="current_password" id="current_password" value="" aria-describedby="currentPasswordHelp" required>
+                <input type="password" class="form-control @error('current_password') is-invalid @enderror" name="current_password" id="current_password" value="" aria-describedby="currentPasswordHelp" required>
 
                 <small id="currentPasswordHelp" class="form-text text-muted"> {{ trans('auth.current_password_help') }} </small>
             </div>
@@ -51,7 +51,7 @@
         <div class="col-md-6">
             <div class="form-group mt-2">
                 <label for="new_password"> {{ trans('auth.new_password') }} </label>
-                <input type="text" class="form-control @error('new_password') is-invalid @enderror" name="new_password" id="new_password" value="" aria-describedby="newPaswordHelp" required>
+                <input type="password" class="form-control @error('new_password') is-invalid @enderror" name="new_password" id="new_password" value="" aria-describedby="newPaswordHelp" required>
 
                 <small id="newPaswordHelp" class="form-text text-muted"> {{ trans('auth.new_password_help') }} </small>
             </div>
@@ -60,7 +60,7 @@
         <div class="col-md-6">
             <div class="form-group mt-2">
                 <label for="new_password_confirmation"> {{ trans('auth.new_password_confirmation') }} </label>
-                <input type="text" class="form-control @error('new_password_confirmation') is-invalid @enderror" name="new_password_confirmation" id="new_password_confirmation" value="" aria-describedby="newPasswordConfirmationHelp" required>
+                <input type="password" class="form-control @error('new_password_confirmation') is-invalid @enderror" name="new_password_confirmation" id="new_password_confirmation" value="" aria-describedby="newPasswordConfirmationHelp" required>
 
                 <small id="newPasswordConfirmationHelp" class="form-text text-muted"> {{ trans('auth.new_password_confirmation_help') }} </small>
             </div>
